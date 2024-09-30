d5420c005e2da8ce444f3bf6f33f85f7c32ddcfb
tiffany <886fan@gmail.com>
2024-10-01 01:52:54 +0800

    homework 2

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
 
 app = FastAPI()
 
-
+# http://localhost:8080/
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.put("/items/{item_id}")
+async def get_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
